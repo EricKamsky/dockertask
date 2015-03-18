03b6a5b05e882f700483b4941d54d182c42b3976
Jonah Duckles <jonah@duckles.org>
2015-03-18 13:35:13 -0500

    fixing what I messed up

--- docker-task/tasks/tasks.py
+++ docker-task/tasks/tasks.py
@@ -4,11 +4,6 @@ import paramiko as pk
 import os, json
 import getpass
 
-#Example task
-@task()
-def add(x, y):
-    result = x + y
-    return result
 
 def ssh_client(host,port,username):
     """ Create a paramiko connection object and return it """
@@ -40,8 +35,7 @@ def isrunning(state):
     else:
         return False
 
-@task
-def remote_task(
+def docker_task(
     docker_worker=None,
     docker_name=None,
     docker_opts=None,
@@ -97,38 +91,6 @@ def remote_task(
                     return { "host": docker_worker, "task_id": str(task_id) }
                 else:
                     raise Exception(state['Error'])
-    else:
-        raise Exception(std_err)
-
-
-@task()
-def qc_docker_workflow(forward_read_filename, reverse_read_filename, basedir="/data/static/",docker_worker=os.environ['docker_worker']):
-    resultDir = os.path.join(basedir, 'mgmic_tasks/', str(qc_docker_workflow.request.id))
-    os.makedirs(resultDir)
-    ssh = pk.SSHClient()
-    ssh.set_missing_host_key_policy(pk.AutoAddPolicy())
-    #keys setup to login as pass environmental username to docker
-    ssh.connect(docker_worker,22,os.environ['username'])
-    cmd ='docker run -d -v /opt/local/scripts/:/scripts -v /data/static:/data/static bwawrik/bioinformatics /scripts/bin/Illumina_MySeq_Trim %s %s %s'
-    cmd = cmd % (forward_read_filename,reverse_read_filename,resultDir)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    std_out = stdout.read()
-    std_err = stderr.read()
-    if std_err == '':
-        dock_id = std_out.strip(' \n')
-        #Example poll_docker return
-        #{u'Pid': 0, u'OOMKilled': True, u'Paused': False, u'Running': False,
-        # u'FinishedAt': u'2015-03-13T19:11:31.66940599Z', u'Restarting': False,
-        # u'Error': u'', u'StartedAt': u'2015-03-13T19:08:56.879774955Z', u'ExitCode': 0}
-        while True:
-            poll = docker_state(dock_id,ssh)
-            if poll['Running']:
-                sleep(10)
-            else:
-                if poll['ExitCode']==0:
-                    return "http://%s/mgmic_tasks/%s" % (docker_worker,str(qc_docker_workflow.request.id))
-                else:
-                    raise Exception(poll['Error'])
     else:
         raise Exception(std_err)
 
